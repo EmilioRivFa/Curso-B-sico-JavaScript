6d5e5c28304ff05408e8de8a225c59331aca06e5
EmilioRivFa <emiliorivfa@gmail.com>
2025-01-20 15:00:00 -0600

    ejercicio 3

---
+++ ejercicioPais.js
@@ -0,0 +1,13 @@
+
+/*Crea un objeto llamado myCountryel país de tu elección,
+ que contenga las propiedades country, capital, languagey 
+ (una matriz como las populationque neighboursusamos en 
+ tareas anteriores).*/
+const miPais = {
+    pais : 'mexico',
+    capital : 'cdmx',
+    lenguaje : 'español',
+    poblacion : '6666558',
+    vecinos : ['estados_unidos', 'Canada', 'guatamela']
+}
+console.log('mi pais es:' + miPais );
