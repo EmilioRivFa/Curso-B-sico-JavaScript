aac2ade12a3e00018fa29871eb98c6d337f696ed
EmilioRivFa <emiliorivfa@gmail.com>
2025-01-30 15:00:00 -0600

    metodos de objeto

---
+++ metodosDeObjeto.js
@@ -0,0 +1,10 @@
+const miPais = {
+    pais : 'mexico',
+    capital : 'cdmx',
+    lenguaje : 'español',
+    poblacion : '6,000,000',
+    vecinos : ['estados_unidos', 'Canada', 'guatamela']
+    
+}
+miPais
+checkIsland 
