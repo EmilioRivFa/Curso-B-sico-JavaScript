179f7a48192218bf9879a45b2465e4295a49ee9a
EmilioRivFa <emiliorivfa@gmail.com>
2025-02-02 17:45:41 -0600

    Sentencia Do While

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-for(var i=10; i>=1; i++){
+for(var i=10; i>=1; i--){
     console.log(i);
 
 }
+
+/*
+var i = 1;
+/* sentencia While, Do while
+while(i >= 10){
+    console.log(i);
+    i--
+}
+console.log(i);
+
+/*sentencia Do While */
+
+var i = 11;
+do{
+console.log(i)
+i++
+}while(i <=10);
+console.log(i)
